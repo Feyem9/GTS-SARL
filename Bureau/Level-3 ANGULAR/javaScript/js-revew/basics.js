e781fd7ab94bb9832ec0e810ee3cee03e2974bec
FEYEM <feyemlionel@gmail.com>
2023-06-21 21:45:58 +0100

    finish with 4 exercices

---
+++ Bureau/Level-3 ANGULAR/javaScript/js-revew/basics.js
@@ -0,0 +1,42 @@
+// a variable is a container for store data temporarely
+
+let nameOfStudents = "Mimi , Nana , Rian";
+var age = 39_30_10;
+const gender = "FEMALE"
+
+
+// datatype => define the shape of data that we should be used during program
+// PREMITIVE TYPE AND REFERENCE TYPE
+
+
+// Boolean = true || false
+// String  = "......." || '....'
+// Number  = 162636-36363-2-263
+// null
+// object = Array.object
+// function
+// undefined
+// void
+
+// STACK HEAd  
+// an expression =>  An expression is a block of code that evaluates to a value
+let age = (30 + 5); // =>
+
+// operators and simple arithmetics operators => ?? , :, ?
+// ternary operators =>  < ,> ,<= ,>= , ==, ===, ...
+// -= , += , *=
+let  sum = 4;
+let age2 = 3;
+sum+=age2 // sum = sum + age additional assigments operators
+
+
+// REFFERENCE TYPES
+// OBJECTS
+// LITTERAL NOTATION                        // OBJECT CONSTRUCTORS
+let person = {                              //new Numbers()
+    name  : "",                             //new string()
+    age : 27,                               //etc......
+    gender : 'MALE',                        //array && function
+};
+
+// functional programing
